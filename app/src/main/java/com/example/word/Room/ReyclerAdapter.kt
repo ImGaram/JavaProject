335beb8faa6d123acd4cbeb1d51251595b508a8b
imgaram <s21049@gsm.hs.kr>
2021-12-06 21:14:40 +0900

    sixth commit(room 수정)

--- app/src/main/java/com/example/word/Room/ReyclerAdapter.kt
+++ app/src/main/java/com/example/word/Room/ReyclerAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.word.Room
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
--- app/src/main/java/com/example/word/fragment/Fragment3.kt
+++ app/src/main/java/com/example/word/fragment/Fragment3.kt
@@ -5,49 +5,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.word.R
 import com.example.word.Room.RecyclerAdapter
+import com.example.word.Room.RoomHelper
 import com.example.word.Room.RoomMemo
 import com.example.word.Room.RoomMemoDAO
-import kotlinx.android.synthetic.main.activity_fragment3.*
+import com.example.word.databinding.ActivityFragment3Binding
 
 class Fragment3 : Fragment() {
+    val binding by lazy { ActivityFragment3Binding.inflate(layoutInflater) }
     val memoList = mutableListOf<RoomMemo>()
     lateinit var memoDAO: RoomMemoDAO
     lateinit var memoAdapter:RecyclerAdapter
+    lateinit var helper:RoomHelper
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-          //  helper = Room.databaseBuilder(MainPage, RoomHelper::class.java, "room_db")
-            //    .allowMainThreadQueries()
-              //  .build()
-
-        lateinit var helper:com.example.word.Room.RoomHelper
+        val view = inflater.inflate(R.layout.activity_fragment3, container, false)
+        helper = Room.databaseBuilder(requireContext(), RoomHelper::class.java, "room_db")
+            .allowMainThreadQueries()
+            .build()
 
         memoDAO = helper.roomMemoDao()
         memoAdapter = RecyclerAdapter(memoList)
+        refreshAdapter()
 
-        recyclerMemo.adapter = memoAdapter
-       // recyclerMemo.layoutManager = LinearLayoutManager(this)
+        with(binding) {
+            recyclerMemo.adapter = memoAdapter
+            recyclerMemo.layoutManager = LinearLayoutManager(requireContext())
 
-        buttonSave.setOnClickListener {
-            val content = editMemo.text.toString()
-            if(content.isNotEmpty()) {
-                val datetime = System.currentTimeMillis()
-                val memo = RoomMemo(content, datetime)
-                memoDAO.insert(memo)
+            buttonSave.setOnClickListener {
+                val content = editMemo.text.toString()
+                if(content.isNotEmpty()) {
+                    val datetime = System.currentTimeMillis()
+                    val memo = RoomMemo(content, datetime)
+                    memoDAO.insert(memo)
 
-                refreshAdapter()
+                    refreshAdapter()
+                }
             }
         }
 
-        return inflater.inflate(R.layout.activity_fragment3, container, false)
+        return view
     }
-    fun refreshAdapter(){
 
+    fun refreshAdapter(){
         memoList.clear()
         memoList.addAll(memoDAO.getAll())
         memoAdapter.notifyDataSetChanged()
